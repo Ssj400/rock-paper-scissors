34d6724c650b2d4ffaf1691d2e45a7fa23d67b68
ssj400 <gafetel2017@gmail.com>
2025-01-25 15:23:16 -0300

    Added score and game features
    Now a game consist in 5 rounds, each round gives
    you a score (0 or 1) depending on your result.

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-let userInput = prompt("Select Rock, Paper or Scissors")
-let engineInput = () => {
-    random = (Math.floor(Math.random() * 3)) + 1;
-    if (random == 1) {
-        return "Rock";
-    } else if (random == 2) {
-        return "Paper"
-    } else if (random == 3) {
-        return "Scissors"
-    }
-}
 
 let humanScore = 0;
 let engineScore = 0;
 
 let playRound = (user1, engine) => {
+    console.log("--------------------------------------------------");
+    if (user1 == null || user1 == "") {
+        console.log("Game cancelled");
+        return null;
+    }
     let user = user1.toLowerCase();
     if (user === "rock" && engine === "Rock") {
         console.log("User: Rock, Engine: Rock, Result: Tie");
@@ -25,26 +19,52 @@ let playRound = (user1, engine) => {
         console.log("User: Scissors, Engine: Scissors, Result: Tie");
 
     } else if (user === "rock" && engine === "Scissors") {
+        humanScore++;
         console.log("User: Rock, Engine: Scissors, Result: You win!");
 
     } else if (user === "rock" && engine === "Paper") {
+        engineScore++;
         console.log("User: Rock, Engine: Paper, Result: Engine wins!");
 
     } else if (user === "paper" && engine === "Scissors") {
+        engineScore++;
         console.log("User: Paper, Engine: Scissors, Result: Engine wins!");
 
     } else if (user === "scissors" && engine === "Rock") {
+        engineScore++;
         console.log("User: Scissors, Engine: Rock, Result: Engine wins!");
 
     } else if (user === "paper" && engine === "Rock") {
+        humanScore++;
         console.log("User: Paper, Engine: Rock, Result: You win!");
 
     } else if (user === "scissors" && engine === "Paper") {
-        console.log("User: Scissors, Engine: Paper, Result: Engine wins!");
+        humanScore++;
+        console.log("User: Scissors, Engine: Paper, Result: You win!");
 
     } else {
         console.log("Please write Rock, Paper or Scissors");
     }
 }
 
-playRound(userInput, engineInput());
+
+let playGame = () => {
+    for (let i = 0; i < 5; i++) {
+        let userInput = prompt("Select Rock, Paper or Scissors")
+        let engineInput = () => {
+            random = (Math.floor(Math.random() * 3)) + 1;
+            if (random == 1) {
+                return "Rock";
+            } else if (random == 2) {
+                return "Paper"
+            } else if (random == 3) {
+                return "Scissors"
+            }
+        }
+
+    playRound(userInput, engineInput());
+    console.log(`Score:  You: ${humanScore} Engine: ${engineScore} `);
+    }
+}
+
+playGame();
