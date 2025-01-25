2f97576e7da554f202a5867b061f69948827c8a8
ssj400 <gafetel2017@gmail.com>
2025-01-25 13:22:31 -0300

    Added userInput and engineInput logic

---
+++ script.js
@@ -0,0 +1,14 @@
+let userInput = prompt("Select Rock, Paper or Scissors")
+let engineInput = () => {
+    random = (Math.floor(Math.random() * 3)) + 1;
+    return random;
+    if (random = 1) {
+
+    } else if (random = 2) {
+        
+    } else if (random = 3) {
+
+    }
+}
+
+console.log(engineInput());
